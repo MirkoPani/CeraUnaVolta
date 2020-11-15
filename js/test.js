c4106881d6d7aa77d48ef5718ff96ef21996832f
MirkoPani <nastykill@gmail.com>
2020-11-15 16:02:36 +0000

    Deploying to gh_pages from  @ a5545d8adc64db95ac83f9fa7a9ec17190dcdb1d 🚀

---
+++ js/test.js
@@ -0,0 +1,11 @@
+﻿export function Call() {
+    var element = document.querySelector('#scene');
+
+    // And pass it to panzoom
+    panzoom(element, {
+        bounds: true,
+        boundsPadding: 1,
+        maxZoom: 4,
+        minZoom: 0.5
+    });
+}

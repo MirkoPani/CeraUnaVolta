8207c0ddf896fa428c69d87ea482e67eb5ecd412
MirkoPani <nastykill@gmail.com>
2020-11-25 21:27:14 +0000

    Deploying to gh_pages from  @ d5c6e390adfe159e93631ad438949612c12c260b 🚀

--- js/test.js
+++ js/test.js
@@ -74,32 +74,32 @@
     // USing Leaflet.Coordinates plugin at https://github.com/MrMufflon/Leaflet.Coordinates
 
      //Patch first to avoid longitude wrapping.
-    L.Control.Coordinates.include({
-        _update: function (evt) {
-            var pos = evt.latlng,
-                opts = this.options;
-            if (pos) {
-                //pos = pos.wrap(); // Remove that instruction.
-                this._currentPos = pos;
-                this._inputY.value = L.NumberFormatter.round(pos.lat, opts.decimals, opts.decimalSeperator);
-                this._inputX.value = L.NumberFormatter.round(pos.lng, opts.decimals, opts.decimalSeperator);
-                this._label.innerHTML = this._createCoordinateLabel(pos);
-            }
-        }
-    });
+    //L.Control.Coordinates.include({
+    //    _update: function (evt) {
+    //        var pos = evt.latlng,
+    //            opts = this.options;
+    //        if (pos) {
+    //            //pos = pos.wrap(); // Remove that instruction.
+    //            this._currentPos = pos;
+    //            this._inputY.value = L.NumberFormatter.round(pos.lat, opts.decimals, opts.decimalSeperator);
+    //            this._inputX.value = L.NumberFormatter.round(pos.lng, opts.decimals, opts.decimalSeperator);
+    //            this._label.innerHTML = this._createCoordinateLabel(pos);
+    //        }
+    //    }
+    //});
 
-    L.control.coordinates({
-        position: "bottomleft",
-        decimals: 0, //optional default 4
-        decimalSeperator: ".", //optional default "."
-        labelTemplateLat: "Latitude: {y}", //optional default "Lat: {y}"
-        labelTemplateLng: "Longitude: {x}", //optional default "Lng: {x}"
-        enableUserInput: true, //optional default true
-        useDMS: false, //optional default false
-        useLatLngOrder: true, //ordering of labels, default false-> lng-lat
-        markerType: L.marker, //optional default L.marker
-        markerProps: {} //optional default {}
-    }).addTo(map);
+    //L.control.coordinates({
+    //    position: "bottomleft",
+    //    decimals: 0, //optional default 4
+    //    decimalSeperator: ".", //optional default "."
+    //    labelTemplateLat: "Latitude: {y}", //optional default "Lat: {y}"
+    //    labelTemplateLng: "Longitude: {x}", //optional default "Lng: {x}"
+    //    enableUserInput: true, //optional default true
+    //    useDMS: false, //optional default false
+    //    useLatLngOrder: true, //ordering of labels, default false-> lng-lat
+    //    markerType: L.marker, //optional default L.marker
+    //    markerProps: {} //optional default {}
+    //}).addTo(map);
 
     function onClick(e) {
         DotNet.invokeMethodAsync('CeraUnaVolta', 'UpdateMessageCaller');

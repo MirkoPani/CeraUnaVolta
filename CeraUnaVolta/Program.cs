555a5d0ecfdeefc3346be5915ede49e30b9019a2
Mirko Pani <panimirko95@gmail.com>
2020-11-26 23:18:14 +0100

    Aggiornato logo Aggiunto servizio per immagini Aggiunta logica per chiamare funzioni con parametri diversi da javascript Nascosti tasti e scritta sulla mappa Migliorata schermata di caricamento iniziale Aggiunto navbar brand

--- CeraUnaVolta/Program.cs
+++ CeraUnaVolta/Program.cs
@@ -1,3 +1,4 @@
+using CeraUnaVolta.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace CeraUnaVolta
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient<IImageService,ImageService>();
 
             await builder.Build().RunAsync();
         }
---
+++ CeraUnaVolta/Services/ImageService.cs
@@ -0,0 +1,37 @@
+﻿using CeraUnaVolta.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeraUnaVolta.Services
+{
+    public class ImageService : IImageService
+    {
+        public List<ImageData> Immagini { get; }
+
+        public ImageService()
+        {
+            Immagini = new List<ImageData>
+            {
+                new ImageData
+                {
+                    Didascalia = "Test didascalia",
+                    Source = "img/test/test-image.jpeg",
+                    Titolo = "Titolo di prova"
+                },
+                new ImageData
+                {
+                    Didascalia = "Discalia di Immagine 2",
+                    Source = "img/test/villa.jpg",
+                    Titolo = "Titolo 2"
+                }
+            };
+
+
+
+        }
+
+    }
+}
